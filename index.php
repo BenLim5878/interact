20df06a2775f75fbd21733f8371bbdc4ae87623c
Ben Lim <limzhengwei1002@gmail.com>
2022-07-15 10:51:31 +0800

    commit

--- index.php
+++ index.php
@@ -8,9 +8,9 @@
   <link rel="preconnect" href="https://fonts.gstatic.com">
   <link href="https://fonts.googleapis.com/css2?family=Catamaran:wght@100;200;300;400;500;600;700;800;900&family=Open+Sans:ital,wght@0,300;0,400;0,600;0,700;0,800;1,300;1,400;1,600;1,700;1,800&display=swap" rel="stylesheet">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/aos/2.3.4/aos.css" integrity="sha512-1cK78a1o+ht2JcaW6g8OXYwqpev9+6GqOkz9xmBN9iUUhIndKtxwILGWYOSibOKjLsEdjyjZvYDq/cZwNeak0w==" crossorigin="anonymous" />
-  <link rel="stylesheet" href="/interact/styles/style.css">
-  <link rel="stylesheet" href="/interact/styles/header.css">
-  <link rel="stylesheet" href="/interact/styles/footer.css">
+  <link rel="stylesheet" href="/styles/style.css">
+  <link rel="stylesheet" href="/styles/header.css">
+  <link rel="stylesheet" href="/styles/footer.css">
   <link rel="shortcut icon" type="image/png" href="materials/logo/logo-transparent.png">
 </head>
 
