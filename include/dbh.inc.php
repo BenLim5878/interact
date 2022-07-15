10a044c2c7049ab8bd4a32e7c95d0d030336ac9f
Ben Lim <limzhengwei1002@gmail.com>
2022-07-15 12:24:17 +0800

    commit

--- include/dbh.inc.php
+++ include/dbh.inc.php
@@ -1,11 +1,13 @@
 <?php
 
-$serverName = "localhost";
-$dBUsername = "root";
-$dBPassword = "";
-$dBName = "interact";
+$url = parse_url(getenv("mysql://b59a876e2981e8:959d7daf@us-cdbr-east-03.cleardb.com/heroku_cd1f104ee4baddf?reconnect=true"));
 
-$conn =  mysqli_connect($serverName, $dBUsername, $dBPassword, $dBName);
+$server = $url["host"];
+$username = $url["user"];
+$password = $url["pass"];
+$db = substr($url["path"], 1);
+
+$conn =  mysqli_connect($server, $username, $password, $db);
 
 if (!$conn) {
   die("Connection failed: " . mysqli_connect_error());
