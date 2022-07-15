914f1b50c45571f4dd6aebb4056d3938e6893f7a
Ben Lim <limzhengwei1002@gmail.com>
2022-07-15 12:36:01 +0800

    commit

--- include/dbh.inc.php
+++ include/dbh.inc.php
@@ -2,10 +2,10 @@
 
 $url = parse_url(getenv("mysql://b59a876e2981e8:959d7daf@us-cdbr-east-03.cleardb.com/heroku_cd1f104ee4baddf?reconnect=true"));
 
-$server = $url["host"];
-$username = $url["user"];
-$password = $url["pass"];
-$db = substr($url["path"], 1);
+$server = "us-cdbr-east-03.cleardb.com";
+$username = "b59a876e2981e8";
+$password = "959d7daf";
+$db = "heroku_cd1f104ee4baddf";
 
 $conn =  mysqli($server, $username, $password, $db);
 
