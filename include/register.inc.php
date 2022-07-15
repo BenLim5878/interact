2b24844c6ff5e66b22ce9e40af214af71c9a9dc5
Ben Lim <limzhengwei1002@gmail.com>
2022-07-15 13:01:27 +0800

    commit

--- include/register.inc.php
+++ include/register.inc.php
@@ -1,4 +1,46 @@
 <?php
 
+if (isset($_POST["submit"])) {
 
-?>
+  // $Fname = $_POST["Fname"];
+  // $Lname = $_POST["Lname"];
+  // $email = $_POST["email"];
+  // $pwd = $_POST["pwd"];
+  // $pwdRepeat = $_POST["pwdrepeat"];
+
+
+  // require_once 'dbh.inc.php';
+  // require_once 'functions.inc.php';
+  // /*error handler*/
+
+  // if (emptyInputRegister($Fname, $Lname, $email, $pwd, $pwdRepeat) !== false) {
+  //   header("location: ../register.php?error=emptyinput");
+  //   exit();
+  // }
+  // if (invalidFname($Fname) !== false) {
+  //   header("location: ../register.php?error=invalidname");
+  //   exit();
+  // }
+  // if (invalidLname($Lname) !== false) {
+  //   header("location: ../register.php?error=invalidname");
+  //   exit();
+  // }
+  // if (invalidEmail($email) !== false) {
+  //   header("location: ../register.php?error=invalidemail");
+  //   exit();
+  // }
+  // if (pwdMatch($pwd, $pwdRepeat) !== false) {
+  //   header("location: ../register.php?error=passwordsdontmatch");
+  //   exit();
+  // }
+  // if (uidExists($conn, $Fname, $Lname, $email) !== false) {
+  //   header("location: ../register.php?error=accountexists");
+  //   exit();
+  // }
+
+  // createUser($conn, $Fname, $Lname, $email, $pwd);
+  echo("fasd");
+} else {
+  // header("location: ../register.php");
+  // exit();
+}
