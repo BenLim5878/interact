08f85248fe326495c197b05964de51d4e06cade4
Ben Lim <limzhengwei1002@gmail.com>
2022-07-15 12:45:34 +0800

    commit

--- include/dbh.inc.php
+++ include/dbh.inc.php
@@ -7,6 +7,15 @@ $username = "b59a876e2981e8";
 $password = "959d7daf";
 $db = "heroku_cd1f104ee4baddf";
 
+try{
+  $conn = new PDO("mysql:host=cdbr-east-03.cleardb.com;dbname=myDB", "b59a876e2981e8", "959d7daf");
+  // set the PDO error mode to exception
+  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  echo "Connected successfully";
+} catch(PDOException $e) {
+  echo "Connection failed: " . $e->getMessage();
+}
+
 $conn =  new mysqli_connect($server, $username, $password, $db);
 
 ?>
