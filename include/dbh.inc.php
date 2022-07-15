e1ca51466888f9a5bfdd7c8a3302aed041fd1170
Ben Lim <limzhengwei1002@gmail.com>
2022-07-15 13:33:14 +0800

    commit

--- include/dbh.inc.php
+++ include/dbh.inc.php
@@ -1,19 +1,18 @@
 <?php
 
 $cleardb_url = parse_url(getenv("mysql://b8085750c4cb45:741318c2@us-cdbr-east-06.cleardb.net/heroku_eed5da92befa448?reconnect=true"));
-$cleardb_server = $cleardb_url["host"];
-$cleardb_username = $cleardb_url["user"];
-$cleardb_password = $cleardb_url["pass"];
-$cleardb_db = substr($cleardb_url["path"],1);
-$active_group = 'default';
-$query_builder = TRUE;
+// $cleardb_server = $cleardb_url["host"];
+// $cleardb_username = $cleardb_url["user"];
+// $cleardb_password = $cleardb_url["pass"];
+// $cleardb_db = substr($cleardb_url["path"],1);
+// $active_group = 'default';
+// $query_builder = TRUE;
 
-Connect to DB
-$conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
+// $conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
 
-if (!$conn){
-  echo mysqli_connect_errno().PHP_EOL;
-  exit();
-}
+// if (!$conn){
+//   echo mysqli_connect_errno().PHP_EOL;
+//   exit();
+// }
 
 ?>
