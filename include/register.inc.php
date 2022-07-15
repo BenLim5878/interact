273c207e446fd6439b2b489c3885cfa70f63316b
Ben Lim <limzhengwei1002@gmail.com>
2022-07-15 13:02:59 +0800

    commit

--- include/register.inc.php
+++ include/register.inc.php
@@ -2,11 +2,11 @@
 
 if (isset($_POST["submit"])) {
 
-  // $Fname = $_POST["Fname"];
-  // $Lname = $_POST["Lname"];
-  // $email = $_POST["email"];
-  // $pwd = $_POST["pwd"];
-  // $pwdRepeat = $_POST["pwdrepeat"];
+  $Fname = $_POST["Fname"];
+  $Lname = $_POST["Lname"];
+  $email = $_POST["email"];
+  $pwd = $_POST["pwd"];
+  $pwdRepeat = $_POST["pwdrepeat"];
 
 
   // require_once 'dbh.inc.php';
