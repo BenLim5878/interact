38324bcd8ae1de5bb18fd7ae2a5dc3ee4e2d15fc
Ben Lim <limzhengwei1002@gmail.com>
2022-07-15 11:17:10 +0800

    commit

--- archive/about/howItWorks1.php
+++ archive/about/howItWorks1.php
@@ -7,10 +7,10 @@
     <title>Rules and Regulations</title>
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css2?family=Catamaran:wght@100;200;300;400;500;600;700;800;900&family=Open+Sans:ital,wght@0,300;0,400;0,600;0,700;0,800;1,300;1,400;1,600;1,700;1,800&display=swap" rel="stylesheet">
-    <link rel="shortcut icon" type="image/png" href="/interact/materials/logo/logo-transparent.png">
-    <link rel="stylesheet" href="/interact/styles/rules-regulations/template.css">
-    <link rel="stylesheet" href="/interact/styles/header.css">
-    <link rel="stylesheet" href="/interact/styles/footer.css">
+    <link rel="shortcut icon" type="image/png" href="/materials/logo/logo-transparent.png">
+    <link rel="stylesheet" href="/styles/rules-regulations/template.css">
+    <link rel="stylesheet" href="/styles/header.css">
+    <link rel="stylesheet" href="/styles/footer.css">
 </head>
 
 <body onload="textArea()">
@@ -18,13 +18,13 @@
         <?php include '../../include/header.php>'; ?>
         <!-- Path links -->
         <div id="pathway-links">
-            <a href="/interact/help.php" id="help-back">Help</a>
+            <a href="/help.php" id="help-back">Help</a>
             <div>
                 <svg id="back-help" xmlns="http://www.w3.org/2000/svg" width="24" height="12" viewBox="0 0 24 15" fill="#EF6C35" style="transform: rotate(90deg);">
                     <path d="M12 0L0 12L2.83 14.83L12 5.66L21.17 14.83L24 12L12 0Z" fill="#EF6C35" />
                 </svg>
             </div>
-            <a href="/interact/about.php" id="rules-back">About</a>
+            <a href="/about.php" id="rules-back">About</a>
         </div>
         <div class="seperator">
             <div class="side-menu">
