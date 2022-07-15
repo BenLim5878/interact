13fa98745b0282a9148010047b5159283a512fd8
Ben Lim <limzhengwei1002@gmail.com>
2022-07-15 13:38:01 +0800

    commit

--- include/dbh.inc.php
+++ include/dbh.inc.php
@@ -8,7 +8,7 @@ $cleardb_db = substr($cleardb_url["path"],1);
 $active_group = 'default';
 $query_builder = TRUE;
 
-$conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
+// $conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
 
 // if (!$conn){
 //   echo mysqli_connect_errno().PHP_EOL;
