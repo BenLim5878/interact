e37995700c6d96b84402581f432d1f322689fce9
Ben Lim <limzhengwei1002@gmail.com>
2022-07-15 12:52:34 +0800

    commit

--- include/dbh.inc.php
+++ include/dbh.inc.php
@@ -2,20 +2,14 @@
 
 $url = parse_url(getenv("mysql://b59a876e2981e8:959d7daf@us-cdbr-east-03.cleardb.com/heroku_cd1f104ee4baddf?reconnect=true"));
 
-$server = "us-cdbr-east-03.cleardb.com";
-$username = "b59a876e2981e8";
-$password = "959d7daf";
-$db = "heroku_cd1f104ee4baddf";
-
-try{
-  $conn = new PDO("mysql:host=cdbr-east-03.cleardb.com;dbname=myDB", "b59a876e2981e8", "959d7daf");
-  // set the PDO error mode to exception
-  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-  echo "Connected successfully";
-} catch(PDOException $e) {
-  echo "Connection failed: " . $e->getMessage();
-}
-
-$conn =  new mysqli_connect($server, $username, $password, $db);
+$cleardb_url = parse_url(getenv($url));
+$cleardb_server = $cleardb_url["host"];
+$cleardb_username = $cleardb_url["user"];
+$cleardb_password = $cleardb_url["pass"];
+$cleardb_db = substr($cleardb_url["path"],1);
+$active_group = 'default';
+$query_builder = TRUE;
+// Connect to DB
+$conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
 
 ?>
